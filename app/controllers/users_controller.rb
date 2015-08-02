a1fe769d22528364b93b68db2b5a4c76ac78905b
akisute3 <akisute3@gmail.com>
2015-08-02 20:37:29 +0900

    Guitar, Drum のスキル表ページ作成

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -9,6 +9,26 @@ class UsersController < ApplicationController
   def show
   end
 
+  def drum
+    @title = 'DrumMania'
+    @skill = []
+    @goal = []
+    @hots = []
+    @others = []
+
+    render 'skill'
+  end
+
+  def guitar
+    @title = 'GuitarFreaks'
+    @skill = []
+    @goal = []
+    @hots = []
+    @others = []
+
+    render 'skill'
+  end
+
   private
     def set_user
       @user = User.find(params[:id])
--- config/routes.rb
+++ config/routes.rb
@@ -2,5 +2,10 @@ Rails.application.routes.draw do
   devise_for :users
   root 'welcome#index'
 
-  resources :users, only: [:index, :show]
+  resources :users, only: [:index, :show] do
+    member do
+      get :drum
+      get :guitar
+    end
+  end
 end
