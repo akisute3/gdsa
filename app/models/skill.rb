1c4640c8b246c412ca530b9e399a34ec8393da75
akisute3 <akisute3@gmail.com>
2015-08-30 19:54:00 +0900

    Skill モデルから不要なメソッド・定数を削除

--- app/models/skill.rb
+++ app/models/skill.rb
@@ -14,9 +14,6 @@ class Skill < ActiveRecord::Base
     numericality: {more_than_or_equal_to: 0, less_than_or_equal_to: 100}
 
 
-  GAMES_GUITAR = ["Guitar", "Base"]
-  GAMES_DRUM = "Drum"
-
   # スキル計算に使用される曲の順位の下限
   HOT_SKILL_LIMIT = OTHER_SKILL_LIMIT = 25
 
@@ -72,29 +69,4 @@ class Skill < ActiveRecord::Base
   def grade
     @grade ||= Grade.from_achievement(achievement)
   end
-
-
-  # save の事前処理追加版
-  def check_and_save
-    # mst_level が存在しなければ nil を返す
-    return nil unless self.mst_level
-
-    # 重複する Skill を削除してから save
-    delete_existing_target
-    save
-  end
-
-  # update の事前処理追加版
-  def check_and_update(skill_params, level_params)
-    # mst_level_id が存在しなければ nil を返す
-    l = MstLevel.find_by(level_params)
-    l ? update(skill_params.merge(mst_level_id: l.id)) : nil
-  end
-
-
-  private
-    def delete_existing_target
-      same_group_ids = self.mst_level.extract_same_group_ids
-      self.class.delete_all(user_id: self.user_id, mst_level_id: same_group_ids)
-    end
 end
