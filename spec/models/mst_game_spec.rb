64a8eace8aec796f1d07e22b97d4492728ce8fbe
akisute3 <akisute3@gmail.com>
2015-09-02 00:18:01 +0900

    MstGame, MstLevel のテスト作成

---
+++ spec/models/mst_game_spec.rb
@@ -0,0 +1,19 @@
+# coding: utf-8
+require 'rails_helper'
+
+RSpec.describe MstGame, type: :model do
+  context 'ゲームに対応した Inst オブジェクトを取得できる' do
+    it 'Drum' do
+      game = MstGame.find_by(name: 'Drum')
+      expect(game.inst.to_sym).to eq :drum
+    end
+    it 'Guitar' do
+      game = MstGame.find_by(name: 'Guitar')
+      expect(game.inst.to_sym).to eq :guitar
+    end
+    it 'Base' do
+      game = MstGame.find_by(name: 'Base')
+      expect(game.inst.to_sym).to eq :guitar
+    end
+  end
+end
---
+++ spec/models/mst_level_spec.rb
@@ -0,0 +1,21 @@
+# coding: utf-8
+require 'rails_helper'
+
+RSpec.describe MstLevel, type: :model do
+  context '難易度の表示用文字列を返すことができる' do
+    it 'Drum は少数第 2 位までの数値' do
+      level = MstLevel.find(1)
+      expect(level.format).to eq '2.30'
+    end
+
+    it 'Guitar は少数第 2 位までの数値の後ろに (G) が付く' do
+      level = MstLevel.find(6)
+      expect(level.format).to eq '4.20 (G)'
+    end
+
+    it 'Base は少数第 2 位までの数値の後ろに (B) が付く' do
+      level = MstLevel.find(10)
+      expect(level.format).to eq '3.70 (B)'
+    end
+  end
+end
