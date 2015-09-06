a90829f657248acec7b1302f5900b70e99af7cef
akisute3 <akisute3@gmail.com>
2015-09-06 17:32:19 +0900

    全曲リストを JSON で取得する機能追加

--- app/controllers/musics_controller.rb
+++ app/controllers/musics_controller.rb
@@ -1,6 +1,24 @@
+# coding: utf-8
 class MusicsController < ApplicationController
   def index
-    @hots = MstMusic.includes(:mst_levels).where(hot: true)
-    @others = MstMusic.includes(:mst_levels).where(hot: false)
+    @hots = MstMusic.includes(mst_levels: [:mst_game, :mst_difficulty]).where(hot: true)
+    @others = MstMusic.includes(mst_levels: [:mst_game, :mst_difficulty]).where(hot: false)
+
+    respond_to do |format|
+      format.html
+      format.json do
+        list = {}
+        (@hots + @others).each do |music|
+          games = Hash.new { |h,k| h[k] = {} }
+          music.mst_levels.each do |l|
+            games[l.mst_game.name][l.mst_difficulty.name] = l.level
+          end
+
+          list[music.name] = games
+        end
+
+        render json: list
+      end
+    end
   end
 end
